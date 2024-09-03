b120dd0e0222fec3e21bb05f3269db5ab36b1077
Bobisbadmand <sean_hamilton_3@yahoo.com>
2024-09-03 00:55:59 -0400

    Ch2 5. Draw Player

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 # throughout this file
 import pygame
 from constants import *
-from pygame.locals import *
+#from pygame.locals import *
+from player import Player
 
 def main():
     pygame.init()
@@ -13,12 +14,14 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen,(0,0,0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         #print(dt)
